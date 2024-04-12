7571221e4ce64d3146cedb8a0c4d9c41c4bf774a
SwapnilAkolkar <click2swapnil@gmail.com>
2024-04-12 10:35:32 +0530

    disabled the access of plugin from home

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -1,14 +1,10 @@
 <?php
 
 namespace App\Controllers;
-use App\Plugins\HelloWorldPlugin\HelloWorldPlugin;
-use App\Plugins\TaskManagerPlugin;
 
 class Home extends BaseController
 {
     public function index()
     {
-        $plugin = new HelloWorldPlugin();
-        echo $plugin->renderHelloWorldMessage();
     }
 }
